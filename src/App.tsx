59dcae1dc5d93347ddb9c3c0123f6725f62f827e
Ritik Chauhan <ritik.chauhan@quokkalabs.com>
2023-09-05 17:45:38 +0530

    (feat): add session storage in redux persist

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,23 @@ import Hooks from "./components/Hooks/Hooks";
 import ThrottledButton from "./components/Hooks/Throttling";
 import Person from "./components/classComponents/Person";
 import { useFetchallDataQuery } from "./redux/api/api";
+import { useDispatch } from "react-redux";
+import { storeLoginToken } from "./redux/slices/authSlice";
+
+//always use 'use client' while using query or mutation
+//use 'use client' in in next js in page.tsx,layout.tsx file in src->app
+//In next.js wrap children like this
+// <body className={inter.className}>
+//       <Provider store={store}>
+//         <PersistGate loading={null} persistor={persistor}>
+//           {children}
+//         </PersistGate>
+//       </Provider>
+//     </body>
 
 function App() {
-  //always use 'use client' while using query or mutation
-  //use 'use client' in in next js in page.tsx,layout.tsx file in src->app
-  //In next.js wrap children like this
-  // <body className={inter.className}>
-  //       <Provider store={store}>
-  //         <PersistGate loading={null} persistor={persistor}>
-  //           {children}
-  //         </PersistGate>
-  //       </Provider>
-  //     </body>
-  
+  const dispatch = useDispatch();
+
   // QUERY FETCHING
   const reqObj = {
     url: "posts",
@@ -24,11 +28,15 @@ function App() {
   const { data } = useFetchallDataQuery(reqObj);
   console.log("data", data);
 
+  //Slice use
+
   return (
     <div className="App">
       <Hooks />
       {/* <ThrottledButton/> */}
       {/* <Person name="Ritik" /> */}
+      <button onClick={() => dispatch(storeLoginToken("123"))}>Login</button>
+      <button onClick={() => dispatch(storeLoginToken(""))}>Logout</button>
     </div>
   );
 }
